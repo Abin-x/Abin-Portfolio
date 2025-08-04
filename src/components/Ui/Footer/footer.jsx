c0b0cdfddd8311912bee399d8e8cc06e396ec44d
Abin <gitz358@gmail.com>
2025-08-05 02:38:28 +0530

    update contact

--- src/components/Ui/Footer/footer.jsx
+++ src/components/Ui/Footer/footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
           <h4>Quick Links</h4>
           <ul>
             <li><a href="#home">Home</a></li>
-            <li><a href="#projects">Projects</a></li>
+            <li><a href="#project">Projects</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
             <li><a href="#services">Services</a></li>
